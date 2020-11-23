5a287c8265ba9d739fa71aa8ea110a57bbe44ed7
uo226650 <uo226650@uniovi.es>
2020-11-24 00:24:59 +0100

    first working version

--- Ejercicio10.js
+++ Ejercicio10.js
@@ -1,44 +1,52 @@
 const API_KEY = "623c8a4d1ed0493b828f265e099c2c7b";
-
+//La API gratuíta solo permite recuperar noticias de hasta un mes de antigüedad.
+//Para acceder a noticias anteriores es necesario pasar a un plan de pago
 class Noticia {
     
     constructor(){
-        this.prueba();
+        this.frecuencia = 7;//Últimos 7 días por defecto
+        //this.prueba();
     }
 
-
-    /*
-    prueba(){
-        var url = 'https://newsapi.org/v2/top-headlines?' +
-        'country=es' +
-        '&apiKey=' + API_KEY;
-        $.getJSON( url )
-            .done(function( json ) {
-                console.log( "JSON Data: " + json.json() );
-                //this.presentaDatos(json);
-            })
-            .fail(function( textStatus, error ) {
-                var err = textStatus + ", " + error;
-                console.log( "Request Failed: " + err );
-      });
-    }*/
-    
-    prueba(){
-        var url = 'https://newsapi.org/v2/top-headlines?' +
-        'country=us' +
-        '&apiKey=' + API_KEY;
+    /**
+     * Muestra la cuenta de artículos totales que contienen la palabra clave
+     * y el titular de aquellos más populares
+     * 
+     * @param {String} palabraClave 
+     */
+    pideDatos(palabraClave){
+        //var today = new Date("2020-11-05");
+        var dias = this.frecuencia;
+        var today = new Date();//new Date().toISOString().slice(0, 10);
+        var from = new Date(today-dias*24*60*60*1000);
+        console.log(today);
+        console.log(from);
+        //var to = "2020-11-05";
+        var urlBusca = "https://newsapi.org/v2/everything?q=" + 
+            palabraClave + "&sortBy=popularity" + 
+            "&from=" + from.toISOString().slice(0, 10) + 
+            "&to=" + today.toISOString().slice(0, 10) + 
+            "&apiKey=" + 
+            API_KEY;
+        //var url = 'https://newsapi.org/v2/top-headlines?' +
+        //'country=us' +
+        //'&apiKey=' + API_KEY;
         $.ajax({
             dataType: "json",
-            url: url,
+            url: urlBusca,
             method: 'GET',
             success: function(datos){
                 var articulos = datos.articles;
-                var parrafo = "<p>";
+                var html = "";
                 articulos.forEach(articulo => {
-                    parrafo += articulo;
+                    html += "<article><h2>"
+                        + "<a href='" + articulo.url + "'>" 
+                        + articulo.title + "</a></h2>"
+                        + "<p>" + articulo.content
+                        + "</p></article>"
                 });
-                parrafo += "</p>";
-                $("main").html(parrafo);
+                $("main").html(html);
+                $("#total").html("<h3>Total: " + datos.totalResults + "</h3>");
 
             },
             error: function () {
@@ -46,11 +54,24 @@ class Noticia {
             }
           });
     }
+
+    limpiarCampo(elemento){
+        document.getElementById(elemento).value = "";
+    }
+
+    setRango(frecuencia){
+        this.frecuencia = frecuencia;
+    }
+
+    buscar(){
+        var palabraClave = document.getElementById("busqueda").value;
+        this.pideDatos(palabraClave);
+    }
     
 
 }
 
-var prueba = new Noticia();
+var noticia = new Noticia();
 
 
 
